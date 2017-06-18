73db56c941e51225b17d007ff20ea531fdba33ba
Alexandre <alexandre.dufeil@insa-lyon.fr>
2017-06-18 11:52:05 +0200

    Added a new parameter to total.sh : simulation time in ms

--- bitpeer fused/total.sh
+++ bitpeer fused/total.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+#Argument 1 : nom du fichier final
+#Argument 2 : temps d'execution en ms
+sed -i '4s/.*/simulation.endtime '$2'/' config-BitTorrent.cfg
 
-make 
+make
 
 make run 1>$1
 
@@ -8,6 +11,4 @@ sh cleaner.sh $1
 
 gnuplot -e "datafile='$1'"  Map2Commandes.gnu
 
-sh mkwebm.sh $1
-
-
+sh mkwebm.sh $1 $2
