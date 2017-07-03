f5105eec6471266788af5750e25c150b62645840
jbtrystram <jbtrystram@redhat.com>
2017-07-03 09:16:59 +0200

    reorganize repo

--- bitpeer fused/mkhistogram.sh
+++
@@ -1,9 +0,0 @@
-javac -d classes src/ParserHistogram.java
-
-cd classes
-
-java ParserHistogram ../output/scripts/webm/$1 | tee ../output/scripts/histogram/data
-
-cd ../output/scripts/histogram/
-
-gnuplot -p -e 'gnuplot'
--- bitpeer fused/output/scripts/webm/cleaner.sh
+++
@@ -1,2 +0,0 @@
-head -n -1 $1 > temp.txt ; mv temp.txt $1
-echo "$(tail -n +2 $1)" > $1
